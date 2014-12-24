532ae5408be43d7216411029ed8cdae8e17c7562
lachgra <lachie.j.g@live.com>
2014-12-24 17:38:27 +1100

    Simplified disconnect of clients

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2013 Lachie Grant <https://github.com/lachgra>
+# Copyright (C) 2014 Lachie Grant <https://github.com/lachgra>
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,22 +15,18 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-#TODO, change the size of the buttons
-
 import pygtk
 pygtk.require("2.0")
 import gtk
 
 import sys
 import socket
-import thread
-import time
+import threading
 import os
+import time
 import gobject
-from utils import serverHelp
 
 gobject.threads_init()
-
 
 vers = "v1.15"
 window = True
@@ -38,41 +34,19 @@ logging = True
 buff = 256
 port = 6667
 
-
-if len(sys.argv) > 1:
-	if "--help" in sys.argv or "-h" in sys.argv:
-		serverHelp()
-		sys.exit()
-
-	if "--version" in sys.argv or "-v" in sys.argv:
-		print "Python Internet Relay Chat Server - pirc %s" % vers
-		sys.exit()
+if len(sys.argv) > 2 and sys.argv[1] == "-s":
+	screen_size = sys.argv[2].split('x')
+	for i in range(2):
+		screen_size[i] = int(screen_size[i])
+	width, height = screen_size
 	
-	if "--no-window" in sys.argv or "-nw" in sys.argv:
-		window = False
+else:
+	width, height = gtk.gdk.screen_get_default().get_width(), gtk.gdk.screen_get_default().get_height()
 
-	if "--no-logging" in sys.argv or "-nl" in sys.argv:
-		logging = False
-
-	if "--buffer-size" in sys.argv:
-		index = sys.argv.index("--buffer-size")
-		buff = int(sys.argv[index+1])	
-
-	if "-bs" in sys.argv:
-		index = sys.argv.index("-bs")
-		buff = int(sys.argv[index+1])
-
-	if "--port" in sys.argv:
-		index = sys.argv.index("--port")
-		port = int(sys.argv[index+1])
-		
-	if "-p" in sys.argv:
-		index = sys.argv.index("-p")
-		port = int(sys.argv[index+1])
-
-	assert type(port) is int
-	assert type(buff) is int
-	
+def serverHelp():
+	print "Usage: python server.py [ARGUMENTS]..."
+	print "-h, --help		print help message"
+	print "-v, --version		print the version"
 
 ## Server class ##
 class Server:
@@ -85,16 +59,16 @@ class Server:
 		self.window = window
 		if window : self.setupGui()
 
-		self.clientmap = {}
-		self.disconnected = 0
-		self.threshold = 10
+		self.clients = []
 
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)		# creates an empty socket
 		self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 	# makes port reusable
 		self.sock.bind((self.addr, self.port))					# binds to the port
 		self.sock.listen(1)							# listens for connections
 
-		thread.start_new_thread(self.clients, ())				# starts the client adding method
+		self.listener = threading.Thread(group=None, target=self.acceptClients, name=None, args=())	# starts the client accepting method
+		self.listener.setDaemon(True)
+		self.listener.start()
 
 		if self.logging: 
 			if not "log" in os.listdir(".") : os.mkdir("log")
@@ -104,71 +78,71 @@ class Server:
 		self.output("Listening on " + repr(self.sock.getsockname()), False)
 		self.output("bfrsize %s, window %s, logging %s" % (str(buff), str(window), str(logging)), False)
 
-		thread.start_new_thread(self.main(), ())
+		self.main()
 
+	# the main thread
 	def main(self):
 		if window : gtk.main()
-		while not self.quit:
-			pass
-		self.shutdown()
+		self.exit()
 		
+	# accepts new clients and associated threads
+	def acceptClients(self):
+		while True:
+			sock, addr = self.sock.accept()
+		
+			c = client(sock, addr)
+			self.clients.append(c)
+			
+			self.output("New socket, %s, %s" % (c.addr[0], c.nick))				# log the new client addition
+			listener = threading.Thread(None, target=self.listen, args=(c,))		# define thread
+			listener.setDaemon(True)							# make thread a daemon
+			listener.start()								# start listening
 
-	# accepts new clients as they send requests, and then creates threads to listen on that socket
-	def clients(self):
-		while not self.quit:
-			try:
-				sock, addr = self.sock.accept()							# accepts a new client
-				index = len(self.clientmap) + self.disconnected					# the client index
-				c = client(index, sock, addr)							# the client instance
-				self.clientmap[index] = c							# add the client to clientmap
-				self.output("New socket, #%s, %s, %s" % (repr(c.index), c.addr[0], c.nick))	# log the new client addition
-				thread.start_new_thread(self.listen, (c,))					# listens on the client socket
-
-			except KeyboardInterrupt:	
-				sys.exit("Exiting: KeyboardInterrupt")
-
-			except socket.error as e:
-				if e.errorno == 32: # broken pipe
-					sys.exit("Exiting: Broken pipe")
-
-	# listen to and distribute replies as they are received
+	# listen to and distribute replies
 	def listen(self, sender):
-		while not self.quit:
+		while True:
 			message = sender.sock.recv(self.buffer_size)
-			if not sender.connected: # to prevent the very last message from getting through
-				break
 			if message == "/close":
-				self.disconnect(sender)
-				return
-			if message == "/exit":
-				self.disconnect(sender)
-				return
+				sender.disconnect()
+				self.output("Disconnected socket, %s, %s" % (sender.addr[0], sender.nick), True)
+				self.clients.remove(sender)
+				break
 			if message == "/help":
 				pass
 			if message == "/msg":
 				pass
-			#self.output(repr(sender.index) + ": " + sender.nick + ": " + message)
 			self.send(message, sender.nick)
 	
 	# reply distribution
 	def send(self, message, nick, details=True):
 		"""Sends data to specified clients (default: All)"""
-		for index in self.clientmap:
-			receiver = self.clientmap[index]		
-			if details:
-				receiver.sock.send(nick + ": " + message)
+		for c in self.clients:
+			if details: 
+				c.sock.send(nick + ": " + message)
 			else:
-				receiver.sock.send(message)
+				c.sock.send(message)
 
 		self.output(nick + ": " + message)
 
-	# disconnect a socket
-	def disconnect(self, client):
-		self.output("Disconnected socket, #%s, %s, %s" % (repr(client.index), client.addr[0], client.nick), False) # not logged to window
-		self.disconnected += 1
-		self.clientmap[client.index].disconnect()
-		del self.clientmap[client.index]
 
+	# executes commands
+	def execute(self, command):
+		if command == 'reboot':
+			self.output("Restarting server...")
+			for c in self.clients:
+				c.disconnect()
+			self.output("Server restarted")
+			
+		elif command == 'help':
+			self.output("Commands are: shutdown, reboot, kick")
+	
+		else:
+			self.output("Invalid command, type help for a list of commands.")
+
+	# broadcast a message to the whole server
+	def broadcast(self, message, to_window_log=True):
+		self.send(message, "SERVER")
+		self.output(message, to_window_log)
 
 	# server-wide automated logger
 	def output(self, log_message, to_window_log=True):
@@ -183,47 +157,38 @@ class Server:
 		else:
 			print log_message
 
-	# executes commands
-	def execute(self, command):
-		if command == 'reboot':
-			self.output("Restarting server...")
+	# kick a player method
+	def kick(self, client):
+		"""Kicks a client, by passing a client instance as an argument."""
+		self.output("Kicking client " + client.nick)
+		self.send("Kicked client " + client.nick, "SERVER")
+		self.disconnect(client)
 
-		elif command.startswith("kick"):
-			index = int(command.strip()[4:])
-			for c in self.clientmap.keys():
-				if c == index:
-					client = self.clientmap[c]
-					self.send("%s has been kicked from the server." %client.nick, "SERVER")
-					self.disconnect(client)
-		elif command == 'help':
-			self.output("Commands are: shutdown, reboot, kick")
-	
-		else:
-			self.output("Invalid command, type help for a list of commands.")
-
+	# print a pretty banner :-)
 	def banner(self):
-		"""Prints a pretty banner"""
 		if "banner.txt" in os.listdir(".") and not window:
 			banner = file("banner.txt", 'r') 
 			for i in banner.readlines():
 				self.output(i)
 			print "\n"
 	
-	# sets up the window
+	# initializes the GUI
 	def setupGui(self):
-		"""
-		The vbox is a verical box (the main box with a scrolled window
-		The sbox is a horizontal box (the button and input box)
-		"""
+		"""Creates a new Gtk Window that can be used to control the server,
+		containing an input line for both messaging and commands, and several
+		controls."""
+
+		"""The vbox is a verical box (the main box with a scrolled window
+		The sbox is a horizontal box (the button and input box)"""
 
 		# create a new window
 		self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-		self.window.resize(600, 600)
+		self.window.resize(800, 600)
 		# set window parameters
 		self.window.set_title("Pirc Daemon")
 		self.window.set_border_width(10)
 
-		# connects the exit button with gtk.main_quit
+		# connects the exit button to gtk.main_quit
 		self.window.connect("delete-event", gtk.main_quit)
 
 		# define and add the vertical box
@@ -249,8 +214,9 @@ class Server:
 
 		# define the command text box
 		self.txtInput = gtk.Entry()
-		self.cmdExecute = gtk.Button("Execute")
+		self.cmdExec = gtk.Button("Execute")
 		self.cmdSend = gtk.Button("Send")
+		self.cmdExit = gtk.Button("Exit")
 		
 		# pack vbox
 		self.vbox.pack_start(self.winScroll, True, True, 0)
@@ -258,27 +224,33 @@ class Server:
 
 		# pack sbox (subset of vbox)	
 		self.sbox.pack_start(self.txtInput, True, True, 0)
-		self.sbox.pack_start(self.cmdExecute, False, False, 0)
+		self.sbox.pack_start(self.cmdExec, False, False, 0)
 		self.sbox.pack_start(self.cmdSend, False, False, 0)
+		self.sbox.pack_start(self.cmdExit, False, False, 0)
 
-		"""
-		Send_handler is the handler ID, used to disconnect or block the handler
+		"""Send_handler is the handler ID, used to disconnect or block the handler
 		of the "clicked" signal, emitted by the cmdSend GtkWidget instance.
-		This method call connected the button signal to the event handler (i.e. the method)
-		"""
+		This method call connected the button signal to the event handler (i.e. the method)"""
 
-		execute_handler_id = self.cmdExecute.connect("clicked", self.btnClick, "cmdExecute")
+		# define handler IDs
+		exec_handler_id = self.cmdExec.connect("clicked", self.btnClick, "cmdExec")
 		send_handler_id = self.cmdSend.connect("clicked", self.btnClick, "cmdSend")
+		exit_handler_id = self.cmdExit.connect("clicked", self.btnClick, "cmdExit")
+
+		# show everything
 		self.window.show_all()
 
+	# a simple button event handler
 	def btnClick(self, widget, args):
+		"""Button handler for the GUI"""
 		if not args:
 			raise ValueError
 
-		if args == "cmdExecute":
+		if args == "cmdExec":
 			command = self.txtInput.get_text()
 			if command != "":
-				self.execute(command)
+				self.execute(command) #TODO
+				self.output(command)
 				self.txtInput.set_text("")
 
 		if args == "cmdSend":
@@ -286,24 +258,29 @@ class Server:
 			if message != "":
 				self.send(message, "SERVER")
 				self.txtInput.set_text("")
-				#self.output("SERVER" + ": " + message)
+		
+		if args == "cmdExit":
+			if window: 
+				gtk.main_quit() # close the window
+			self.quit = True
 
-	def shutdown(self):
-		self.quit = True
-		self.sock.close()
-		print "Goodbye!"
-		sys.exit()
+	# an exit method
+	def exit(self):
+		self.broadcast("Goodbye!")
+		for c in self.clients:
+			c.disconnect()
+		self.sock.close() 		# close main socket
+		exit()	 			# finally exit the terminal
 
 
 ## client class ##
 class client:
-	"""Represents a client as an attribute of the server"""
-	def __init__(self, index, sock, addr):
-		self.index = index
+	"""Represents a client, used as an attribute of the server instance."""
+	def __init__(self, sock, addr):
 		self.sock = sock
 		self.addr = addr
 		self.nick = self.nickname()
-		self.connected = True
+		
 
 	def nickname(self):
 		while True:
@@ -311,11 +288,7 @@ class client:
 
 	def disconnect(self):
 		self.sock.close()
-		self.connected = False
-			
-
-
+		
 ## __main__ ##
 if __name__ == "__main__":
 	s = Server(window, logging, buff, port)
-
